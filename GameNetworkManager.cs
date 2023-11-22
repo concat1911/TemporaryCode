831aa3127df2e237550339943b2bef35ea03eecf
Tran Nhat Linh <49843263+concat1911@users.noreply.github.com>
2023-11-22 07:34:26 +0700

    Update GameNetworkManager.cs

--- GameNetworkManager.cs
+++ GameNetworkManager.cs
@@ -1,93 +1,102 @@
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using FishNet;
+using FishNet.Managing.Scened;
+using FishNet.Object;
 
-public class GameNetworkManager : NetworkBehaviour
+namespace VeryDisco.MFPS
 {
-	public static GameNetworkManager instance { get; private set; }
-	public PlayerInstance localPlayerInstance;
-	[SerializeField] private List<PlayerInstance> activePlayers = new();
-	public List<PlayerInstance> ActivePlayers => activePlayers;
+	using System.Collections.Generic;
+	using UnityEngine;
 
-	public Action<PlayerInstance> OnPlayerConnected;
-	public Action<PlayerInstance> OnPlayerDisconnected;
-		
-	public bool IsAllPlayerReady()
+	public class GameNetworkManager : NetworkBehaviour
 	{
-		bool valid = true;
+		public static GameNetworkManager instance { get; private set; }
+		public PlayerInstance localPlayerInstance;
+		[SerializeField] private List<PlayerInstance> activePlayers = new();
+		public List<PlayerInstance> ActivePlayers => activePlayers;
 
-		for (int i = 0; i < activePlayers.Count; i++)
+		public Action<PlayerInstance> OnPlayerConnected;
+		public Action<PlayerInstance> OnPlayerDisconnected;
+		
+		public bool IsAllPlayerReady()
 		{
-			if (activePlayers[i].isReady == false)
+			bool valid = true;
+
+			for (int i = 0; i < activePlayers.Count; i++)
 			{
-				valid = false;
-				break;
+				if (activePlayers[i].isReady == false)
+				{
+					valid = false;
+					break;
+				}
+			}
+
+			return valid;
+		}
+
+		private void Awake()
+		{
+			if (instance != null)
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			instance = this;
+		}
+		
+		public void RegisterLocalPlayer(PlayerInstance player)
+		{
+			localPlayerInstance = player;
+		}
+		
+		public void Register(PlayerInstance player)
+		{
+			if (activePlayers.Contains(player)) return;
+			
+			activePlayers.Add(player);
+			OnPlayerConnected?.Invoke(player);
+		}
+
+		public void UnRegister(PlayerInstance player)
+		{
+			if (activePlayers.Contains(player))
+			{
+				activePlayers.Remove(player);
+				OnPlayerDisconnected?.Invoke(player);
 			}
 		}
 
-		return valid;
-	}
-
-	private void Awake()
-	{
-		if (instance != null)
+		public void LoadLevel()
 		{
-			Destroy(gameObject);
-			return;
+			LoadScence("LevelTest");
+			UnLoadScene("Hub");
+
+			InstanceFinder.SceneManager.OnLoadEnd += OnLoadEnd;
 		}
 
-		instance = this;
-	}
-		
-	public void RegisterLocalPlayer(PlayerInstance player)
-	{
-		localPlayerInstance = player;
-	}
-		
-	public void Register(PlayerInstance player)
-	{
-		if (activePlayers.Contains(player)) return;
-			
-		activePlayers.Add(player);
-		OnPlayerConnected?.Invoke(player);
-	}
-
-	public void UnRegister(PlayerInstance player)
-	{
-		if (activePlayers.Contains(player))
+		void OnLoadEnd(SceneLoadEndEventArgs args)
 		{
-			activePlayers.Remove(player);
-			OnPlayerDisconnected?.Invoke(player);
+			InstanceFinder.SceneManager.OnLoadEnd -= OnLoadEnd;
+
+			localPlayerInstance.SpawnController();
 		}
-	}
-		
-// I have problem here
-	public void LoadLevel()
-	{
-		LoadScence("LevelTest");
-		UnLoadScene("Hub");
 
-		InstanceFinder.SceneManager.OnLoadEnd += OnLoadEnd;
-	}
-
-	void OnLoadEnd(SceneLoadEndEventArgs args)
-	{
-		InstanceFinder.SceneManager.OnLoadEnd -= OnLoadEnd;
-
-		localPlayerInstance.SpawnController();
-	}
-
-	void LoadScence(string sceneName)
-	{
-		if (IsServer == false) return;
+		void LoadScence(string sceneName)
+		{
+			if (IsServer == false) return;
 			
-		SceneLoadData sceneLoadData = new(sceneName);
-		InstanceFinder.SceneManager.LoadGlobalScenes(sceneLoadData);
-	}
+			SceneLoadData sceneLoadData = new(sceneName);
+			InstanceFinder.SceneManager.LoadGlobalScenes(sceneLoadData);
+		}
 
-	void UnLoadScene(string sceneName)
-	{
-		if (IsServer == false) return;
-		SceneUnloadData sceneLoadData = new(sceneName);
-		InstanceFinder.SceneManager.UnloadGlobalScenes(sceneLoadData);
+		void UnLoadScene(string sceneName)
+		{
+			if (IsServer == false) return;
+			SceneUnloadData sceneLoadData = new(sceneName);
+			InstanceFinder.SceneManager.UnloadGlobalScenes(sceneLoadData);
+		}
 	}
 }
+
+
